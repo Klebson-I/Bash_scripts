c499b549ec4c07d0022ddc64115f0a10cdd474e7
Łukasz Klęba <lukasz.kleba99@onet.pl>
2024-01-28 11:23:25 +0100

    write array to split fileName and rename it

--- renameFile.sh
+++ renameFile.sh
@@ -1,7 +1,13 @@
 path=$1
+suffix=$2
 
 if [[ -z $path ]];then
     echo No path given, exit script
+    exit 0
+fi
+
+if [[ -z $suffix ]];then
+    echo No suffix given, exit script
     exit 0
 fi
 
@@ -15,6 +21,10 @@ for fileOrDir in ${lsResult[@]};do
     fi
 done
 
-
+for file in ${onlyFiles[@]};do
+    formatted_input=$(echo "$file" | tr '.' ' ')
+    read -a splitFile <<<"$formatted_input"
+    mv "${path}/$file" "${path}/${splitFile[0]}${suffix}.${splitFile[1]}"
+done
 
 echo ${onlyFiles[@]}
